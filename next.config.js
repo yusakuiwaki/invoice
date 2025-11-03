cdbda14fc706056b0755a2fcfb473a3f7db0904e
yusakuiwaki <189yusaku@gmail.com>
2025-11-03 12:40:21 +0900

    chore: exceljsの削除

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ["exceljs"],
-  },
 };
 
 module.exports = nextConfig;
-
